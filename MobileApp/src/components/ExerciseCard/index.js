a0b28ac8a6cc8e7f09534a0961f48b53f7ed84bc
Fatih Taner <ibrahimfatihtaner@gmail.com>
2024-05-24 22:35:43 +0300

    Style updates

--- MobileApp/src/components/ExerciseCard/index.js
+++ MobileApp/src/components/ExerciseCard/index.js
@@ -1,12 +1,40 @@
 import { LinearGradient } from "expo-linear-gradient";
-import React from "react";
+import React, { useEffect } from "react";
 import { Dimensions, Text, TouchableOpacity, View } from "react-native";
 import styles from "./styles.js";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import Button from "../Button/index.js";
 
 const ExerciseCard = ({ day, exercises }) => {
   const navigation = useNavigation();
   console.log(exercises, "exercises");
+  const {userData} = useSelector((state) => state.userData);
+  console.log(userData, "userData");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/exercise/myExercises", userData.id ).then((response) => {
+      console.log(response.data, "asd");
+    });
+  }, [userData.id]);
+
+  console.log(userData.id, "userData.id")
+
+  const handleFetch = () => {
+    axios.get("http://192.168.1.37:5000/api/exercise/myExercises", {
+      params: {
+      patientId: userData.id,
+      }
+    })
+   .then((response) => {
+      console.log(response.data, "asd");
+    })
+   .catch((error) => {
+      console.error("Error fetching active exercise:", error);
+    });
+  };
+  
   return (
     <LinearGradient
       start={[0, 1]}
@@ -17,6 +45,7 @@ const ExerciseCard = ({ day, exercises }) => {
       <View style={styles.date}>
         <Text style={styles.exerciseText}>{day}</Text>
       </View>
+      <Button title="Fetch" onPress={handleFetch} />
       <View>
         {exercises.map((exercise, index) => (
           <View key={index} style={styles.detail}>
