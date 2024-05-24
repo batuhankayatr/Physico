847425a92157fad45106b2c1b9ee638617856ef5
Fatih Taner <ibrahimfatihtaner@gmail.com>
2024-05-24 03:38:39 +0300

    Fetch Fruit method added

--- MobileApp/src/redux/store.js
+++ MobileApp/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import fetchWeatherReducer from "./features/fetchWeather";
 import userDataReducer from "./features/userData";
+import fetchFruitReducer from "./features/fetchFruit";
 
 export default configureStore({
   reducer: {
     fetchWeather: fetchWeatherReducer,
     userData: userDataReducer,
+    fetchFruit: fetchFruitReducer,
   },
 });
